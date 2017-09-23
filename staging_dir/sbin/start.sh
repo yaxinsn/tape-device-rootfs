6596848037b77271c054df7f4d2bd335941e9475
yaxinsn <yaxinsn@163.com>
2017-09-23 16:25:46 +0800

    modify start.sh

--- staging_dir/sbin/start.sh
+++ staging_dir/sbin/start.sh
@@ -1,4 +1,7 @@
 
 # get topdir
+TOPDIR=/home/root/rundir/
+PERL=${TOPDIR}/usr/bin/lua SRCDIR=/ ~/rundir/usr/sbin/lighttpd -f ~/rundir/etc/lighttpd.conf -m ${TOPDIR}/usr/lib
+
+
  
-export PERL=/usr/bin/lua ;export SRCDIR=/; ~/rundir/bin/lighttpd -f ~/rundir/etc/lighttpd.conf
